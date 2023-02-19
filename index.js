12e367e5f444d0bceec64d54c4b2551a8f837e9c
Aijanyl Elukenova <ajowka05@gmail.com>
2023-02-19 20:21:50 +0600

    Find a loop that prints the squares of numbers from 1 to 10.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function myName(name, surname) {
 
 myName("Aijanyl ", "Elukenova");
 
+// example 2
 pi = 3.14;
 function areaReg(h, r) {
   let result = (3 / 1) * pi * r ** r * h;
@@ -12,12 +13,13 @@ function areaReg(h, r) {
 }
 areaReg(3, 2);
 
+// example 3
 function Lengthstr(str) {
   console.log("Длина словы " + str + " - " + str.length);
 }
 Lengthstr("Kyrgyzstan");
 
-
+// example 4
 function printEvenAndOdd(num) {
   if (num % 2 === 0) {
     console.log(num + " - Even ");
@@ -28,7 +30,7 @@ function printEvenAndOdd(num) {
 }
 printEvenAndOdd(12);
 
-
+// example 5
 function squareNum(a) {
   return a * a;
 }
@@ -58,3 +60,10 @@ for(let i = 1; i <= 8; i++) {
   factorial *= i;
 }
 console.log(factorial);
+
+// example 10
+let sqrt = 1;
+for (let i = 1; i <= 10; i++) {
+  sqrt = i ** 2;
+  console.log(sqrt);
+}
