f27faa241aecd2e6fb32d1fd2922ef62e0bcfb9b
Aijanyl Elukenova <ajowka05@gmail.com>
2023-02-19 20:02:59 +0600

    find the volume of the cone and write it as a function

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function myName(name, surname) {
 }
 
 myName("Aijanyl ", "Elukenova");
+
+pi = 3.14;
+function areaReg(h, r) {
+  let result = (3 / 1) * pi * r ** r * h;
+  console.log("Объем конуса " + result);
+}
+areaReg(3, 2);
